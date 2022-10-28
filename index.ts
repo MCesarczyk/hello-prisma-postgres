501f5dcd62aacd581732e525c4ccf94e4d81ec41
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2022-10-28 10:56:28 +0200

    add users with details to db

--- index.ts
+++ index.ts
@@ -2,13 +2,54 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+interface IUser {
+  name: string;
+  email: string;
+};
+
+interface IUserWithDetails extends IUser {
+  posts: {
+    create: {
+      title: string;
+    },
+  },
+  profile: {
+    create: {
+      bio: string;
+    },
+  },
+};
+
 const fetchAllUsers = async () => {
   const allUsers = await prisma.user.findMany();
   console.log(allUsers);
 };
 
+const addUserWithDetails = async (user: IUserWithDetails) => {
+  await prisma.user.create({ data: user })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
 async function main() {
-  fetchAllUsers();
+  // fetchAllUsers();
+
+  addUserWithDetails({
+    name: 'Alice',
+    email: 'alice@prisma.io',
+    posts: {
+      create: { title: 'Hello World' },
+    },
+    profile: {
+      create: { bio: 'I like turtles' },
+    },
+  });
 };
 
 main()
